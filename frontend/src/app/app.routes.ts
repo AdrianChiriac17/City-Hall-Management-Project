6ed00a74935ace189e3efb03cc8effcf026f25fd
dghiluta26 <denis.ghiluta@gmail.com>
2026-05-11 22:41:40 +0300

    Integrate demo Angular frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    redirectTo: ''
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+
+  // Placeholder routes pentru paginile informative.
+  {
+    path: 'about-us',
+    component: HomeComponent
+  },
+  {
+    path: 'privacy-policy',
+    component: HomeComponent
+  },
+  {
+    path: 'terms-and-conditions',
+    component: HomeComponent
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+}
